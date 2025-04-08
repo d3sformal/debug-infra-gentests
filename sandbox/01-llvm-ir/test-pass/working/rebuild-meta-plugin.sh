e680058cc93bb7c519b39c67683511ee749667e6
Roman Vasut <40754203+BohdanQQ@users.noreply.github.com>
2025-04-08 12:15:07 +0200

    add and "integrate" a helper script

--- sandbox/01-llvm-ir/test-pass/working/rebuild-meta-plugin.sh
+++ sandbox/01-llvm-ir/test-pass/working/rebuild-meta-plugin.sh
@@ -1,3 +1,12 @@
+#!/bin/sh
+set -x
+
+# either run this (rebuilds every time :( )
+#cd ../../custom-metadata-pass
+#./cp-to-llvm-proj.sh
+#cd ../../build
+# or this
 cd ../../../build
+
 ninja
 cd ../01-llvm-ir/test-pass/working
