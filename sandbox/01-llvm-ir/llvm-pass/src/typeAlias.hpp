b3efe8d48f8a3c31612173697fa98a33271c02fb
Roman Vasut <40754203+BohdanQQ@users.noreply.github.com>
2025-05-26 22:34:53 +0200

    add tuple type alias

--- sandbox/01-llvm-ir/llvm-pass/src/typeAlias.hpp
+++ sandbox/01-llvm-ir/llvm-pass/src/typeAlias.hpp
@@ -8,6 +8,7 @@
 #include <map>
 #include <optional>
 #include <set>
+#include <tuple>
 #include <utility>
 
 using u64 = uint64_t;
@@ -28,6 +29,7 @@ template <class T> using Set = std::set<T>;
 template <class K, class V> using Map = std::map<K, V>;
 template <class T, size_t N> using Arr = std::array<T, N>;
 template <class T1, class T2> using Pair = std::pair<T1, T2>;
+template <class... T> using Tuple = std::tuple<T...>;
 using Str = std::string;
 
 #endif // LLCPASS_ALIASES
