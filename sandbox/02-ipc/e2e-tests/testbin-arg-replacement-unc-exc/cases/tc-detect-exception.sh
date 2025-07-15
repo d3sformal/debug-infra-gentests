c1f8bfbed0660608239cc9066ac9597418a18052
Roman Vasut <40754203+BohdanQQ@users.noreply.github.com>
2025-07-15 16:55:29 +0200

    document exception test case

--- sandbox/02-ipc/e2e-tests/testbin-arg-replacement-unc-exc/cases/tc-detect-exception.sh
+++ sandbox/02-ipc/e2e-tests/testbin-arg-replacement-unc-exc/cases/tc-detect-exception.sh
@@ -3,6 +3,7 @@ set -e
 
 Out=$(echo "$1" | tail -n+2)
 # exception is detected inside the try block, and only inside of it
+# we replace 4-th call with index 4 argument (from 5th recorded call)
 echo "$Out" | grep "4|4|Exception"
 
 set +e
