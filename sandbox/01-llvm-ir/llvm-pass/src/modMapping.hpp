5d61ff7c246ced2751dec9b3520ab6aa97840805
Roman Vasut <40754203+BohdanQQ@users.noreply.github.com>
2025-08-12 12:12:17 +0200

    merge mapping and encoding to modMapping, perform slight refactoring

--- sandbox/01-llvm-ir/llvm-pass/src/modMapping.hpp
+++ sandbox/01-llvm-ir/llvm-pass/src/modMapping.hpp
@@ -10,22 +10,29 @@
 #include <utility>
 #include <vector>
 
+// generates function IDs and maps the ID to the user-readable identifier
+// the instances are indended to be used on a per-module basis (a module
+// corresponds to at most one instance of FunctionIDMapper)
 class FunctionIDMapper {
   using SizeList = std::vector<std::pair<size_t, LlcapSizeType>>;
   using FunctionData =
       std::tuple<std::string, llcap::FunctionId,
                  std::vector<std::pair<size_t, LlcapSizeType>>>;
-  llcap::ModuleId ModuleIntId{0};
-  std::string FullModuleId;
-  std::string OutFileName;
-  std::vector<FunctionData> FunctionIds;
-  llcap::FunctionId FunctionId{0};
-  static constexpr size_t SHA256_BYTES = 32;
-
-  static std::array<uint8_t, sizeof(llcap::ModuleId)>
-  collapseHash(const std::array<uint8_t, SHA256_BYTES> &Data);
+  llcap::ModuleId m_moduleIntId{0};
+  std::string m_fullModuleId;
+  std::string m_outFileName;
+  std::vector<FunctionData> m_functionIds;
+  // auto-incrementing ID
+  llcap::FunctionId m_functionIdGenerator{0};
 
 public:
+  static constexpr size_t SHA256_BYTES = 32;
+  using FullHashT = std::array<uint8_t, SHA256_BYTES>;
+  using ShortHashT = std::array<uint8_t, sizeof(llcap::ModuleId)>;
+
+  // flush the mapper, writing recorded mapping into the TargetDir,
+  // discarding the mapper in the process
+  // creates a file correspodning to the module's shortened ID
   [[nodiscard]] static bool flush(FunctionIDMapper &&Mapper,
                                   const std::string &TargetDir);
 
@@ -33,13 +40,13 @@ public:
   llcap::FunctionId addFunction(const std::string &FnInfo,
                                 ClangMetadataToLLVMArgumentMapping &Mapping);
   [[nodiscard]] const std::string &getFullModuleId() const {
-    return FullModuleId;
+    return m_fullModuleId;
   }
   [[nodiscard]] const std::string &getModuleMapId() const {
-    return OutFileName;
+    return m_outFileName;
   }
   [[nodiscard]] llcap::ModuleId getModuleMapIntId() const {
-    return ModuleIntId;
+    return m_moduleIntId;
   }
 };
 
