b534744f8d15ef470654db6a0a7c643e8dff66bd
Roman Vasut <40754203+BohdanQQ@users.noreply.github.com>
2025-08-12 12:13:34 +0200

    llvm-pass: add constants

--- sandbox/01-llvm-ir/llvm-pass/src/constants.hpp
+++ sandbox/01-llvm-ir/llvm-pass/src/constants.hpp
@@ -9,6 +9,9 @@ constexpr std::size_t MODULE_ID_BYTE_SIZE = 4;
 constexpr std::size_t BYTE_ENCODING_SIZE = 2;
 constexpr std::size_t MODULE_ID_CHAR_SIZE =
     MODULE_ID_BYTE_SIZE * BYTE_ENCODING_SIZE;
+constexpr std::size_t BYTE_BITS = 8;
+constexpr std::size_t MODID_BITSIZE = sizeof(llcap::ModuleId) * BYTE_BITS;
+constexpr std::size_t FUNID_BITSIZE = sizeof(llcap::ModuleId) * BYTE_BITS;
 } // namespace llcap
 
 #endif
