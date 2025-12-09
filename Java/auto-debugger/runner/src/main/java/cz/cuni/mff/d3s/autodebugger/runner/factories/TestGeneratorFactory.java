6d00d2d6b7bdc221c9ed5816712c6728ed08af67
Denis Leskovar <leskovar.denis@seznam.cz>
2025-12-09 08:39:04 +0100

    feat(runner): route trace-based strategies based on TraceMode

--- Java/auto-debugger/runner/src/main/java/cz/cuni/mff/d3s/autodebugger/runner/factories/TestGeneratorFactory.java
+++ Java/auto-debugger/runner/src/main/java/cz/cuni/mff/d3s/autodebugger/runner/factories/TestGeneratorFactory.java
@@ -2,6 +2,7 @@ package cz.cuni.mff.d3s.autodebugger.runner.factories;
 
 import cz.cuni.mff.d3s.autodebugger.model.common.RunConfiguration;
 import cz.cuni.mff.d3s.autodebugger.model.common.TargetLanguage;
+import cz.cuni.mff.d3s.autodebugger.model.common.TraceMode;
 import cz.cuni.mff.d3s.autodebugger.model.java.JavaRunConfiguration;
 import cz.cuni.mff.d3s.autodebugger.runner.strategies.TestGenerationStrategyProvider;
 import cz.cuni.mff.d3s.autodebugger.testgenerator.common.LLMConfiguration;
@@ -11,6 +12,7 @@ import cz.cuni.mff.d3s.autodebugger.testgenerator.common.AnthropicClient;
 import cz.cuni.mff.d3s.autodebugger.testgenerator.java.llm.PromptBuilder;
 import cz.cuni.mff.d3s.autodebugger.testgenerator.java.llm.CodeValidator;
 import cz.cuni.mff.d3s.autodebugger.testgenerator.java.trace.NaiveTraceBasedGenerator;
+import cz.cuni.mff.d3s.autodebugger.testgenerator.java.trace.TemporalTraceBasedGenerator;
 import lombok.extern.slf4j.Slf4j;
 
 import java.nio.file.Path;
@@ -73,14 +75,22 @@ public class TestGeneratorFactory {
                     return llmGenerator;
 
                 } else if (strategyId.startsWith("trace-based")) {
-                    // Create the trace-based test generator
+                    // Route based on TraceMode: TEMPORAL mode uses TemporalTraceBasedGenerator
+                    if (runConfiguration.getTraceMode() == TraceMode.TEMPORAL) {
+                        log.info("Temporal trace mode detected, using TemporalTraceBasedGenerator");
+                        TemporalTraceBasedGenerator generator = new TemporalTraceBasedGenerator();
+                        log.info("Successfully created TemporalTraceBasedGenerator for strategy: {}", strategyId);
+                        return generator;
+                    }
+
+                    // NAIVE mode (default) uses NaiveTraceBasedGenerator
                     // Use provided identifier mapping path if available, otherwise fall back to default location
                     Path identifiersPath = identifierMappingPath != null
                             ? identifierMappingPath
                             : javaRunConfiguration.getSourceCodePath().resolve("identifiers");
                     NaiveTraceBasedGenerator generator = new NaiveTraceBasedGenerator(identifiersPath);
 
-                    log.info("Successfully created Java test generator with strategy: {}", strategyId);
+                    log.info("Successfully created NaiveTraceBasedGenerator for strategy: {}", strategyId);
                     return generator;
                 } else {
                     throw new UnsupportedOperationException("Test generation technique not yet supported: " + strategyId);
